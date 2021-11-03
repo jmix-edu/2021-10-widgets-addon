b171b90918ae8c1b5c6db3bd239ed0ea6451fb2b
Gleb Gorelov <g.gorelov@haulmont.com>
2021-11-03 14:28:19 +0400

    Initial Commit

---
+++ src/main/java/com/company/dwa/client/ColorButtonState.java
@@ -0,0 +1,8 @@
+package com.company.dwa.client;
+
+import io.jmix.ui.widget.client.button.JmixButtonState;
+
+public class ColorButtonState extends JmixButtonState {
+
+    public String color = "";
+}
